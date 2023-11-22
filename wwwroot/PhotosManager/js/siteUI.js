b2e2f98109de5260035239fe6a5f02f07c2d1cb9
SimonLerouxx <95500169+SimonLerouxx@users.noreply.github.com>
2023-11-22 16:35:35 -0500

    navigation inscription login

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -17,6 +17,10 @@ function restoreContentScrollPosition() {
 function updateHeader() {
     //todo
 }
+
+
+
+
 function renderAbout() {
     timeout();
     saveContentScrollPosition();
@@ -41,3 +45,114 @@ function renderAbout() {
             </div>
         `))
 }
+
+function renderLogin(){
+
+    eraseContent();
+    $("#content").append(
+
+        $(`
+        <h3></h3>
+        <form class="form" id="loginForm">
+            <input type='email' name='Email' id="Email" class="form-control" required RequireMessage='Veuillez entrer votre courriel' InvalidMessage='Courriel invalide' placeholder="adresse de courriel" value=''>
+            <span style='color:red'></span>
+            <input type='password' name='Password' id="Password" placeholder='Mot de passe' class="form-control" required RequireMessage='Veuillez entrer votre mot de passe'>
+            <span style='color:red'></span>
+            <input type='button' name='submit' id="submit" value="Entrer" class="form-control btn-primary">
+        </form>
+        <div class="form">
+            <hr>
+            <button class="form-control btn-info" id="createProfilCmd">Nouveau compte</button>
+        </div>
+    </div>
+
+`));
+
+var createAccount = document.getElementById("createProfilCmd");
+
+createAccount.addEventListener("click", function(){
+
+    renderInscription();
+
+});
+
+}
+
+function renderInscription() {
+
+    eraseContent();
+    $("#content").append(
+
+        $(`
+            < form class= "form" id = "createProfilForm" >
+        <fieldset>
+<legend>Adresse ce courriel</legend>
+<input type="email"
+class="form-control Email"
+name="Email"
+id="Email"
+placeholder="Courriel"
+required
+RequireMessage = 'Veuillez entrer votre courriel'
+InvalidMessage = 'Courriel invalide'
+CustomErrorMessage ="Ce courriel est déjà utilisé"/>
+<input class="form-control MatchedInput"
+type="text"
+matchedInputId="Email"
+name="matchedEmail"
+id="matchedEmail"
+placeholder="Vérification"
+required
+RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+InvalidMessage="Les courriels ne correspondent pas" />
+</fieldset>
+<fieldset>
+<legend>Mot de passe</legend>
+<input type="password"
+class="form-control"
+name="Password"
+id="Password"
+placeholder="Mot de passe"
+required
+RequireMessage = 'Veuillez entrer un mot de passe'
+InvalidMessage = 'Mot de passe trop court'/>
+<input class="form-control MatchedInput"
+type="password"
+matchedInputId="Password"
+name="matchedPassword"
+id="matchedPassword"
+placeholder="Vérification" required
+InvalidMessage="Ne correspond pas au mot de passe" />
+</fieldset>
+<fieldset>
+<legend>Nom</legend>
+<input type="text"
+class="form-control Alpha"
+name="Name"
+id="Name"
+placeholder="Nom"
+required
+RequireMessage = 'Veuillez entrer votre nom'
+InvalidMessage = 'Nom invalide'/>
+</fieldset>
+<fieldset>
+<legend>Avatar</legend>
+<div class='imageUploader'
+newImage='true'
+controlId='Avatar'
+imageSrc='images/no-avatar.png'
+waitingImage="images/Loading_icon.gif">
+</div>
+</fieldset>
+<input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+</form>
+<div class="cancel">
+<button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+</div>
+
+`));
+
+abort = document.getElementById("abortCmd");
+abort.addEventListener("click", function(){renderLogin(); });
+}
+
