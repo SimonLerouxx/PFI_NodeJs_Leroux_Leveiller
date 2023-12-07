c38ec49ec0da742fd811664d0c38420414e0b9e2
NicLeveille <nicleveille@hotmail.ca>
2023-12-06 23:04:09 -0500

    glitch

--- wwwroot/PhotosManager/js/api.js
+++ wwwroot/PhotosManager/js/api.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////// API photos_APIs call ///////////////////////////////////////////////////////
 
-const serverHost = "http://localhost:5000";
+const serverHost = "https://scented-better-sedum.glitch.me";
 const photos_API = "/api/photos";
 
     
